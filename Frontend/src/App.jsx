a2ca07dcd5eb2e9292e39b2f4ea8158dc82f21e4
Umaid Hamid <umaid.uk39@gmail.com>
2025-12-18 16:16:37 +0530

    Context Api
    check

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import Verify from "./pages/Verify";
-import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-// import SetPassword from "./pages/setPassword";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useAppContext } from "./ContextApi/isAuth.js";
+import { useAuth } from "./ContextApi/isAuth";
+
 const App = () => {
-  const { role, isAuth } = useAppContext();
+  const { role, isAuth } = useAuth(); // ✅ FIX HERE
+
   return (
     <div>
       <ToastContainer
         position="top-center"
         autoClose={2000}
-        hideProgressBar={true}
-        closeOnClick
-        // pauseOnHover
-        draggable
+        hideProgressBar
         theme="dark"
       />
+
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="set-password" element={<Verify />} />
+        <Route path="/set-password" element={<Verify />} />
       </Routes>
     </div>
   );
